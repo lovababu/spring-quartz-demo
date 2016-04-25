d37d618690ef33e31318b10b95104c0bb86acb11
Durga,Padala <durga.padala@neustar.biz>
2016-04-25 18:43:27 +0530

    BlockingDQueue as message transfer object.

--- src/main/java/org/avol/spring/quartz/jobs/MsgReceiverJob.java
+++ src/main/java/org/avol/spring/quartz/jobs/MsgReceiverJob.java
@@ -3,12 +3,23 @@ package org.avol.spring.quartz.jobs;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.util.StopWatch;
+
+import java.util.concurrent.BlockingDeque;
 
 /**
  * Created by Durga on 4/25/2016.
  */
 public class MsgReceiverJob extends QuartzJobBean {
+
+    private BlockingDeque<String> blockingDeque;
+
+    public BlockingDeque<String> getBlockingDeque() {
+        return blockingDeque;
+    }
+
+    public void setBlockingDeque(BlockingDeque<String> blockingDeque) {
+        this.blockingDeque = blockingDeque;
+    }
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
@@ -19,14 +30,15 @@ public class MsgReceiverJob extends QuartzJobBean {
         Long startTime = System.currentTimeMillis();
         Long threadId = Thread.currentThread().getId();
         System.out.println("Current Thread ID: " + threadId);
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Receiving msg id: " + i + " by ThreadId: " + threadId);
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println("Receiving msg id: " + i + " by ThreadId: " + threadId);*/
             try {
+                System.out.println(threadId + " Receiving msg: "+ blockingDeque.take());
                 Thread.sleep(1000);//
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+        /*}*/
         System.out.println("Total time take by threadId:" + threadId + " is " + (System.currentTimeMillis() - startTime));
     }
 }
--- src/main/java/org/avol/spring/quartz/jobs/MsgSenderJob.java
+++ src/main/java/org/avol/spring/quartz/jobs/MsgSenderJob.java
@@ -5,10 +5,24 @@ import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.util.StopWatch;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
 /**
  * Created by Durga on 4/25/2016.
  */
 public class MsgSenderJob extends QuartzJobBean {
+
+    private BlockingDeque<String> blockingDeque;
+
+    public BlockingDeque<String> getBlockingDeque() {
+        return blockingDeque;
+    }
+
+    public void setBlockingDeque(BlockingDeque<String> blockingDeque) {
+        this.blockingDeque = blockingDeque;
+    }
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         System.out.println("MsgSenderJob.executeInternal");
@@ -21,14 +35,15 @@ public class MsgSenderJob extends QuartzJobBean {
         System.out.println("Current Thread ID: " + threadId);
         StopWatch stopWatch = new StopWatch();
         stopWatch.start("Sender");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Sending msg id: " + i + " by Thread: " + threadId);
+        /*for (int i = 0; i < 10; i++) {*/
+            /*System.out.println("Sending msg id: " + i + " by Thread: " + threadId);*/
             try {
+                blockingDeque.put(threadId + " Says Hello");
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+        /*}*/
         System.out.println("Total time take by threadId:" + threadId + " is " + (System.currentTimeMillis() - startTime));
     }
 }
